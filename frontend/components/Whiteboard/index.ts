7fc311f356fc20196fc3573ee946dde66b135864
MoTareq <mohamadtareq954@gmail.com>
2026-09-19 21:22:02 +0300

    Fix index.ts exports

--- frontend/components/Whiteboard/index.ts
+++ frontend/components/Whiteboard/index.ts
@@ -1,4 +1,2 @@
 export { default as Whiteboard } from "./Whiteboard";
-export { default as AnimationEngine } from "./AnimationEngine";
 export { default as AITeacherAvatar } from "./AITeacherAvatar";
-export { WhiteboardLessonLibrary } from "./WhiteboardLessonLibrary";
